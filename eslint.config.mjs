189445de9c5e28f52723f6121ec6896991b82833
bKoZii <bigkonkamon@gmail.com>
2025-01-15 12:17:01 +0700

    style(eslint.config): :sparkles: Added eqeqeq rule & other rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,11 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt({
   rules: {
     'tailwindcss/no-custom-classname': 'off',
-    'vue/no-empty-component-block': 'warn',
+    'vue/no-empty-component-block': 'error',
+    'vue/block-order': ['error', { order: ['template', 'script', 'style'] }],
+    'vue/eqeqeq': ['error', 'always'],
+    'eqeqeq': ['error', 'always'],
+    'vue/max-attributes-per-line': ['error', { singleline: 1, multiline: { max: 1 } }],
   },
 },
 ).prepend(...tailwindcss.configs['flat/recommended'], json.configs['flat/base'])
